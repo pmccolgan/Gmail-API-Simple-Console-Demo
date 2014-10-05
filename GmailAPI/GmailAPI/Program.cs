e0ab0c9755df92617b34db2cd6580eb65eb3294c
Patrick McColgan <patrickmccolgan@patricks-mbp.home>
2014-10-05 09:18:56 +0100

    Working version

--- GmailAPI/GmailAPI/Program.cs
+++ GmailAPI/GmailAPI/Program.cs
@@ -20,6 +20,11 @@ namespace GmailAPI
             var clientId = ConfigurationManager.AppSettings["ClientId"];
             var clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
 
+			var SenderName = ConfigurationManager.AppSettings["EmailSenderName"];
+			var SenderAddress = ConfigurationManager.AppSettings["EmailSenderAddress"];
+			var ReceiverName = ConfigurationManager.AppSettings["EmailReceiverName"];
+			var ReceiverAddress = ConfigurationManager.AppSettings["EmailReceiverAddress"];
+
             try
             {
                 var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
@@ -28,7 +33,7 @@ namespace GmailAPI
                         ClientId = clientId,
                         ClientSecret = clientSecret,
                     },
-                    new[] { GmailService.Scope.GmailReadonly },
+                    new[] { GmailService.Scope.GmailCompose },
                     "user",
                     CancellationToken.None).Result;
 
@@ -38,41 +43,46 @@ namespace GmailAPI
                     ApplicationName = "Gmail API",
                 });
 
-                //var message = new Message();
-                //message.Payload = new MessagePart();
-                //message.Payload.Body = new MessagePartBody();
-                //message.Payload.Body.Data = "";
+				// only the raw parameter of the message resource needs set, see: http://stackoverflow.com/questions/24460422/how-to-send-a-message-successfully-using-the-new-gmail-rest-api
+				// from that there is a working example in the RFC 2822nspecification: "From: John Doe <jdoe@machine.example>\nTo: Mary Smith <mary@example.net>\nSubject: Saying Hello\nDate: Fri, 21 Nov 1997 09:55:06 -0600\nMessage-ID: <1234@local.machine.example>\n\nThis is a message just to say hello.\nSo, \"Hello\".";
+				// use that as a working base
+				// the values Date and Message-ID have no bearing on the final email (or didn't to me) so have keep them as placeholders and haven't tried to replace them
 
-                //message.Payload.Body.da
+				var subject = "Email Subject";
 
-                //// use special value me to send from authenticated account
-                //service.Users.Messages.Send(message, "LentBot@gmail.com").Execute();
+				// there are some issues around the body encoding/decoding
+				// this message decoded will have a '5' at the end
+				// a full stop at the end will make an invalid raw parameter
+				// but this was good enough for my purposes...
+				var body = "Hello this is an email wriien by a very simple console application";
 
-                var query = service.Users.Messages.List("LentBot@gmail.com");
+				// format the message
+				var text = string.Format("From: {0} <{1}>\nTo: {2} <{3}>\nSubject: {4}\nDate: Fri, 21 Nov 1997 09:55:06 -0600\nMessage-ID: <1234@local.machine.example>\n\n{5}", 
+					SenderName,
+					SenderAddress,
+					ReceiverName,
+					ReceiverAddress,
+					subject,
+					body);
 
-                var mail = query.Execute();
+				Console.WriteLine("Send email:\n{0}", text);
 
-                int badger = 7;
+				// must be base64 encoded but also web safe and also initially UTF8
+				// http://stackoverflow.com/questions/24460422/how-to-send-a-message-successfully-using-the-new-gmail-rest-api
+				// http://stackoverflow.com/questions/13017703/java-and-net-base64-conversion-confusion
+				var encodedText = System.Web.HttpServerUtility.UrlTokenEncode(System.Text.Encoding.UTF8.GetBytes(text));
 
-                //var queryStart = DateTime.Now;
-                //var queryEnd = queryStart.AddYears(1);
+				Console.WriteLine("Raw email:\n{0}", encodedText);
 
-                //var query = service.Events.List(calendarId);
-                //// query.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime; - not supported :(
-                //query.TimeMin = queryStart;
-                //query.TimeMax = queryEnd;
+				var message = new Message();
 
-                //var events = query.Execute().Items;
+				message.Raw = encodedText;
 
-                //var eventList = events.Select(e => new Tuple<DateTime, string>(DateTime.Parse(e.Start.Date), e.Summary)).ToList();
-                //eventList.Sort((e1, e2) => e1.Item1.CompareTo(e2.Item1));
+				var request = service.Users.Messages.Send(message, "me").Execute();
 
-                //Console.WriteLine("Query from {0} to {1} returned {2} results", queryStart, queryEnd, eventList.Count);
-
-                //foreach (var item in eventList)
-                //{
-                //    Console.WriteLine("{0}\t{1}", item.Item1, item.Item2);
-                //}
+				Console.WriteLine(
+					string.IsNullOrEmpty(request.Id) ? "Issue sending, returned id: {0}" : "Email looks good, id populated: {0}", 
+					request.Id);
             }
             catch (Exception e)
             {
@@ -84,6 +94,14 @@ namespace GmailAPI
             while (!Console.KeyAvailable)
             {
             }
+        }
+
+        static byte[] GetBytes(string str)
+        {
+			// From http://stackoverflow.com/questions/472906/converting-a-string-to-byte-array
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,18 +1,51 @@
-#Twitter API Simple Console Demo
+#Gmail API Simple Console Demo
 
-Simple console application to use the GMail API to send an email.
+Simple console application to use the GMail API to send an email, couldn't get a C# example of this anywhere and it didn't seem clear what needed to be done but the following sources are what got it going:
+
+What needs done
+http://stackoverflow.com/questions/24460422/how-to-send-a-message-successfully-using-the-new-gmail-rest-api
+
+.Net web safe UTF-8 encoding
+http://stackoverflow.com/questions/13017703/java-and-net-base64-conversion-confusion
+
+Test encoded messages with the Gmail API Reference:
+https://developers.google.com/gmail/api/v1/reference/users/messages/send#try-it
+
+Not perfect has some issues around the encoding but as an example it works.
 
 To use set up application credentials for an installed application (this will produce a Client ID and Client Secret): https://developers.google.com/google-apps/calendar/instantiate
 
 Get the API packages to build: https://www.nuget.org/packages/Google.Apis.Gmail.v1/
 
-Replace the three keys in the app.config:
+Replace the six keys in the app.config:
 ```
-        <add key="ClientId" value="YOUR_CLIENT_ID" />
-        <add key="ClientSecret" value="YOUR_CLIENT_SECRET" />
-        <add key="CalendarId" value="YOUR_CALENDAR_ID" />???????????????????????????????
+    <add key="ClientId" value="YOUR_CLIENT_ID" />
+    <add key="ClientSecret" value="YOUR_CLIENT_SECRET" />
+    <add key="EmailSenderName" value="NAME_TO_APPEAR_FOR_EMAIL_SENDER" />
+    <add key="EmailSenderAddress" value="EMAIL_ADDRESS_FOR_EMAIL_SENDER" />
+    <add key="EmailReceiverName" value="NAME_TO_APPEAR_FOR_EMAIL_RECEIVER" />
+    <add key="EmailReceiverAddress" value="EMAIL_ADDRESS_FOR_EMAIL_RECEIVER" />
 ```
 
-When first run you may be prompted to grant calendar access to the application (this appears in a browser), approve this and the calendar should be readable?????????????????????????????????
+When first run you may be prompted to grant compose email access to the application (this appears in a browser), approve this
 
-http://webstackoflove.com/read-google-gmail-using-dot-net-api-client-library-for-csharp/
+Want to read email instead?
+
+Change scope for access to
+```
+	GmailService.Scope.GmailModify
+```
+
+And try something like:
+```
+	var query = service.Users.Messages.List("me");
+
+	var mail = query.Execute();
+
+	foreach (var item in mail.Messages)
+	{
+	    Message massage = service.Users.Messages.Get("me", item.Id).Execute();
+
+	     Console.WriteLine("Message dump: {0}", massage.Payload.Body.Data);
+	}
+```
